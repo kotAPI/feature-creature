2840e42b8d584a121a5948678c6aded93cab40c4
Pranay <pranay@govisually.com>
2019-07-06 10:43:51 +0530

    init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FeatureEditTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FeatureEditTable from "./components/FeatureEditTable.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    FeatureEditTable
+  },
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/FeatureEditTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <table class="ui celled table">
+      <thead>
+        <tr>
+          <th>&nbsp;</th>
+          <th v-for="(feature,fX) in features" :key="fX">{{feature}}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td data-label="Name">James</td>
+          <td data-label="Age">24</td>
+          <td data-label="Job">Engineer</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="ui action input">
+      <input type="text" placeholder="Search..." v-model="newFeatureName" />
+      <button class="ui labeled icon button" @click="addNewFeature">
+        <i class="add icon"></i>
+        Add Plan
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newFeatureName: "",
+      features: [],
+      plans: ["1"]
+    };
+  },
+  methods: {
+    addNewFeature() {
+      this.features.push(this.newFeatureName);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
